8e9d7ac019552e26371bb572807b86efc828e030
Jeff McCune <jeff@puppetlabs.com>
2014-01-13 14:50:41 -0800

    WIP WHERE I LEFT OFF
    This is just a work in progress of where I left off.  This branch will
    be rebased.
    
    Where I left off was using the new API that I hope will be merged in as
    https://github.com/puppetlabs/puppet/pull/2236
    
    In addition, we'll need to implement the fallback behavior of modifying
    run_mode.rb directly which I consider the "hacky" solution, but it's
    necessary for versions of Puppet that are released today without the
    public relocation API.

--- lib/puppet_transplant/installer.rb
+++ lib/puppet_transplant/installer.rb
@@ -5,10 +5,26 @@ require 'rubygems/installer'
 module PuppetTransplant
   class Installer
     ##
+    # post_install takes a {Gem::Installer} instance expected to be passed from
+    # a {Gem.post_install} callback and modifies an installed `puppet` gem to
+    # use a relocated confdir and vardir.  This method does nothing if the
+    # gem_installer instance is not processing puppet.
+    #
+    # @param [Gem::Installer] gem_installer The gem installer instance of the
+    #   gem just installed, passed from the Gem.post_install callback.
+    def self.post_install(gem_installer)
+      return unless gem_installer.spec.name == 'puppet'
+      installer = new(gem_installer)
+      installer.relocate_puppet!
+    end
+
+    ##
     # pre_install takes a {Gem::Installer} instance expected to be passed from
     # a Gem.pre_install callback and overrides the
     # {Gem::Installer#app_script_text} method if the puppet gem is about to be
     # installed.
+    #
+    # @deprecated in favor of {.post_install}
     #
     # @param [Gem::Installer] gem_installer The gem installer instance of the
     #   gem just installed, passed from the Gem.pre_install callback.
@@ -34,8 +50,16 @@ module PuppetTransplant
     #
     # 1. If `--confdir` or `--vardir` are specified via ARGV then use the
     # provided values.
-    # 2. Otherwise modify ARGV to include `--confdir=/etc/#{org}` and
-    # `--vardir=/var/lib/#{org}`
+    # 2. Otherwise modify ARGV to include `--confdir=/etc/#{org}/puppet` and
+    # `--vardir=/var/lib/#{org}/puppet`
+    #
+    # @deprecated in favor of overriding the default and confdir in a manner
+    #   that supports consistency between puppet as a library and puppet as an
+    #   application.  Implementing a wrapper script that sets --confdir and
+    #   --vardir creates inconsistent behavior between puppet as a library and
+    #   puppet as an application.
+    #
+    # @see {#relocate_puppet!}
     #
     # @see #org
     #
@@ -66,6 +90,76 @@ module PuppetTransplant
       end
     end
 
+    ##
+    # relocate_puppet! overrides the default confdir and vardir using one of
+    # two methods.  First, if the version of Puppet being relocated provides
+    # the Puppet::Util::RunMode.override_path method then the default path will
+    # be overriden using this method.
+    #
+    # If the override_path API is not implemented in the version of Puppet
+    # installed then the run_mode.rb file will be modified directly after
+    # creating a backup at run_mode.rb.orig
+    #
+    # @api private
+    def relocate_puppet!
+      if puppet_override_api?
+        write_override_files
+      else
+        modify_run_mode_in_place
+      end
+    end
+
+    ##
+    # write_override_files writes files to the filesystem using the
+    # {Puppet::Util::RunMode.override_path) public API method to determine
+    # which files to write.  These files are intended to override the default
+    # confdir and vardir for Puppet both as an application and as a library.
+    # See {https://tickets.puppetlabs.com/browse/PUP-1406}
+    #
+    # @api private
+    def write_override_files
+      confdir_path = Puppet::Util::RunMode.override_path('confdir')
+      msg = "# Automatically overriden by the puppet_transplant gem"
+      File.open(confdir_path, "w+") do |f|
+        f.puts(confdir)
+        f.puts(msg)
+      end
+
+      vardir_path = Puppet::Util::RunMode.override_path('vardir')
+      File.open(vardir_path, "w+") do |f|
+        f.puts(vardir)
+        f.puts(msg)
+      end
+    
+      gem_installer.ui.alert "PuppetTransplant modified the system confdir and vardir defaults:"
+      gem_installer.ui.alert "confdir='#{confdir}'"
+      gem_installer.ui.alert "vardir='#{vardir}'"
+    end
+
+    ##
+    # puppet_override_api? determines if the version of Puppet being processed
+    # supports the {Puppet::Util::RunMode.override_path} API which makes it
+    # easier to override the default confdir and vardir.
+    #
+    # @return [Boolean] if puppet supports the override_path API return true.
+    #   if not return false.
+    def puppet_override_api?
+      require 'puppet/util/run_mode'
+      Puppet::Util::RunMode.respond_to?(:override_path)
+    end
+
+    ##
+    # @deprecated in favor of overriding the default and confdir in a manner
+    #   that supports consistency between puppet as a library and puppet as an
+    #   application.  Implementing a wrapper script that sets --confdir and
+    #   --vardir creates inconsistent behavior between puppet as a library and
+    #   puppet as an application.
+    #
+    # @see {#relocate_puppet!}
+    #
+    # @api private
+    #
+    # @return [String] the binstub contents
     def app_script_text(bin_file_name)
       return <<-TEXT
 #{shebang bin_file_name}
@@ -98,7 +192,8 @@ TEXT
     def org
       return @org if @org
       path = Pathname.new(prefix)
-      @org = path.expand_path.to_s.split('/')[2] || 'unknown'
+      idx = is_windows ? 1 : 2
+      @org = path.expand_path.to_s.split('/')[idx] || 'unknown'
     end
 
     ##
@@ -127,12 +222,12 @@ TEXT
     private :shebang
 
     ##
-    # windows? returns true if the ruby interpreter is currently running on a
+    # is_windows returns true if the ruby interpreter is currently running on a
     # windows platform.
-    def windows?
+    def is_windows
       !!(RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
     end
-    private :windows?
+    private :is_windows
 
     def confdir
       @confdir ||= "/etc/#{org}/puppet"
