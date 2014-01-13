02a70d3637ff2d1ec142d0befec1e334172618e3
Jeff McCune <jeff@puppetlabs.com>
2014-01-13 14:50:41 -0800

    Use RunMode to relocate default system confdir and vardir consistently
    Without this patch the puppet_transplant gem relocates Puppet using a
    wrapper script method.  This is a problem because it introduces an
    inconsistency between how Puppet behaves when used as an application and
    how puppet behaves when used as a library.  Specifically, using Puppet
    as a library will use the hard-coded default system confdir and vardir
    while using it as an application will use the overriden default system
    confdir and vardir.
    
    This patch addresses the problem by making sure Puppet behaves
    consistently across use cases and scenarios.  If the version of Puppet
    being installed supports the Puppet::Util::RunMode.override_path Public
    API, then that API will be used to override the default system confdir
    and vardir.  Otherwise, the puppet_transplant gem will fall back to the
    "hack" of directly modifying `run_mode.rb` to change the default system
    confdir and vardir.

--- lib/puppet_transplant/installer.rb
+++ lib/puppet_transplant/installer.rb
@@ -4,11 +4,34 @@ require 'rubygems/installer'
 
 module PuppetTransplant
   class Installer
+    class RelocationError < Exception; end
+
+    ##
+    # post_install takes a {Gem::Installer} instance expected to be passed from
+    # a {Gem.post_install} callback and modifies an installed `puppet` gem to
+    # use a relocated confdir and vardir.  This method does nothing if the
+    # gem_installer instance is not processing puppet.
+    #
+    # @param [Gem::Installer] gem_installer The gem installer instance of the
+    #   gem just installed, passed from the Gem.post_install callback.
+    def self.post_install(gem_installer)
+      # Do nothing unless we're dealing with Puppet.
+      return unless gem_installer.spec.name == 'puppet'
+      # Perform the relocation
+      installer = new(gem_installer)
+      installer.relocate_puppet!
+      # Let the user know what happened
+      gem_installer.ui.debug "Transplanted confdir: #{installer.confdir}"
+      gem_installer.ui.debug "Transplanted vardir:  #{installer.vardir}"
+    end
+
     ##
     # pre_install takes a {Gem::Installer} instance expected to be passed from
     # a Gem.pre_install callback and overrides the
     # {Gem::Installer#app_script_text} method if the puppet gem is about to be
     # installed.
+    #
+    # @deprecated in favor of {.post_install}
     #
     # @param [Gem::Installer] gem_installer The gem installer instance of the
     #   gem just installed, passed from the Gem.pre_install callback.
@@ -34,8 +57,16 @@ module PuppetTransplant
     #
     # 1. If `--confdir` or `--vardir` are specified via ARGV then use the
     # provided values.
-    # 2. Otherwise modify ARGV to include `--confdir=/etc/#{org}` and
-    # `--vardir=/var/lib/#{org}`
+    # 2. Otherwise modify ARGV to include `--confdir=/etc/#{org}/puppet` and
+    # `--vardir=/var/lib/#{org}/puppet`
+    #
+    # @deprecated in favor of overriding the default and confdir in a manner
+    #   that supports consistency between puppet as a library and puppet as an
+    #   application.  Implementing a wrapper script that sets --confdir and
+    #   --vardir creates inconsistent behavior between puppet as a library and
+    #   puppet as an application.
+    #
+    # @see {#relocate_puppet!}
     #
     # @see #org
     #
@@ -66,6 +97,138 @@ module PuppetTransplant
       end
     end
 
+    ##
+    # relocate_puppet! overrides the default confdir and vardir using one of
+    # two methods.  First, if the version of Puppet being relocated provides
+    # the Puppet::Util::RunMode.override_path method then the default path will
+    # be overriden using this method.
+    #
+    # If the override_path API is not implemented in the version of Puppet
+    # installed then the run_mode.rb file will be modified directly after
+    # creating a backup at run_mode.rb.orig
+    #
+    # @return [Boolean] true if Puppet has been successfully relocated, false
+    #   otherwise
+    #
+    # @api public
+    def relocate_puppet!
+      if puppet_override_api?
+        rval = write_override_files
+      else
+        begin
+          rval = modify_run_mode_in_place
+        rescue RelocationError => detail
+          gem_installer.ui.alert_error "Modification of run_mode.rb failed: #{detail}"
+          return false
+        end
+      end
+      !!rval
+    end
+
+    ##
+    # modify_run_mode_in_place directly modifies Puppet's `run_mode.rb` file to
+    # change the default system confdir and vardir.  This behavior is a
+    # fallback mechanism in the event Puppet does not support the so-called
+    # relocation API submitted at
+    # https://github.com/puppetlabs/puppet/pull/2236
+    #
+    # @api private
+    #
+    # @raise [RelocationError] if the method could not perform the relocation.
+    #   The reason will be included in the exception message.
+    #
+    # @see #relocate_puppet!
+    # @see #puppet_override_api?
+    #
+    # @return [Boolean] true if `run_mode.rb` was successfully modified.
+    def modify_run_mode_in_place
+      # Locate the run_mode.rb file
+      dir = gem_installer.dir
+      spec = gem_installer.spec
+
+      if run_mode_file = spec.files.find() {|p| p.match(/\/run_mode.rb$/)}
+        run_mode_path = Pathname.new(File.join(dir, run_mode_file))
+      else
+        raise RelocationError, "Could not find run_mode.rb in the file list."
+      end
+
+      if not run_mode_path.readable?
+        raise RelocationError, "#{run_mode_path} is not readable."
+      end
+
+      if not run_mode_path.writable?
+        raise RelocationError, "#{run_mode_path} is not writable."
+      end
+
+      # This file is small enough to fit into memory.
+      data = File.read(run_mode_path)
+
+      # Modify the file in place for confdir
+      if not data.gsub!(/\/etc\/puppet\b/, confdir)
+        raise RelocationError, "Found no occurrences of /etc/puppet to replace."
+      end
+      # Modify the file in place for vardir
+      if not data.gsub!(/\/var\/lib\/puppet\b/, vardir)
+        raise RelocationError, "Found no occurrences of /var/lib/puppet to replace."
+      end
+
+      # Write the file back out to the system.
+      File.open(run_mode_path, 'w') do |file|
+        file.write(data)
+      end
+      return true
+    end
+
+    ##
+    # write_override_files writes files to the filesystem using the
+    # {Puppet::Util::RunMode.override_path) public API method to determine
+    # whch files to write.  These files are intended to override the default
+    # confdir and vardir for Puppet both as an application and as a library.
+    # See {https://tickets.puppetlabs.com/browse/PUP-1406}
+    #
+    # @api private
+    def write_override_files
+      confdir_path = Puppet::Util::RunMode.override_path('confdir')
+      vardir_path = Puppet::Util::RunMode.override_path('vardir')
+      msg = "# Automatically overriden by the puppet_transplant gem"
+
+      File.open(confdir_path, "w") do |f|
+        f.puts(confdir)
+        f.puts(msg)
+      end
+
+      File.open(vardir_path, "w") do |f|
+        f.puts(vardir)
+        f.puts(msg)
+      end
+
+      return true
+    end
+
+    ##
+    # puppet_override_api? determines if the version of Puppet being processed
+    # supports the {Puppet::Util::RunMode.override_path} API which makes it
+    # easier to override the default confdir and vardir.
+    #
+    # @return [Boolean] if puppet supports the override_path API return true.
+    #   if not return false.
+    def puppet_override_api?
+      require 'puppet/util/run_mode'
+      Puppet::Util::RunMode.respond_to?(:override_path)
+    end
+
+    ##
+    # @deprecated in favor of overriding the default and confdir in a manner
+    #   that supports consistency between puppet as a library and puppet as an
+    #   application.  Implementing a wrapper script that sets --confdir and
+    #   --vardir creates inconsistent behavior between puppet as a library and
+    #   puppet as an application.
+    #
+    # @see {#relocate_puppet!}
+    #
+    # @api private
+    #
+    # @return [String] the binstub contents
     def app_script_text(bin_file_name)
       return <<-TEXT
 #{shebang bin_file_name}
@@ -98,7 +261,8 @@ TEXT
     def org
       return @org if @org
       path = Pathname.new(prefix)
-      @org = path.expand_path.to_s.split('/')[2] || 'unknown'
+      idx = is_windows ? 1 : 2
+      @org = path.expand_path.to_s.split('/')[idx] || 'unknown'
     end
 
     ##
@@ -127,22 +291,38 @@ TEXT
     private :shebang
 
     ##
-    # windows? returns true if the ruby interpreter is currently running on a
+    # is_windows returns true if the ruby interpreter is currently running on a
     # windows platform.
-    def windows?
+    def is_windows
       !!(RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
     end
-    private :windows?
+    private :is_windows
 
+    ##
+    # confdir returns the path of the relocated default system confdir
+    #
+    # @api public
+    #
+    # @see #vardir
+    #
+    # @return [String] The fully qualified path of the relocated default system
+    #   confdir.
     def confdir
       @confdir ||= "/etc/#{org}/puppet"
     end
-    private :confdir
 
+    ##
+    # vardir returns the path of the relocated default system vardir
+    #
+    # @api public
+    #
+    # @see #confdir
+    #
+    # @return [String] The fully qualified path of the relocated default system
+    #   vardir.
     def vardir
       @vardir ||= "/var/lib/#{org}/puppet"
     end
-    private :vardir
 
     ##
     # bindir returns the bin directory where binstub scripts will be written.
